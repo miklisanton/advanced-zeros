e16abf6ffed3ccbcb7b62ba6d26eeccff144689d
Anton <miklis2003@mail.ru>
2019-02-19 18:34:15 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let z=0;
+  for(let i=1;i<number;i++){
+  z=Math.floor(number/((base*0.5)**i))+z;}
+  return z;
 }
